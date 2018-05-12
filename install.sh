0539c7959eb6380b9fa51a5b5b8e4883511a6c8e
gjh14 <gjh14@tsinghua.edu.cn>
2018-05-12 20:53:30 +0800

    modify download address

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ cd ..
 
 cd examples
 mkdir digit
-wget -P digit https://www.kaggle.com/c/3004/download/train.csv
+wget -P digit "https://storage.googleapis.com/kaggle-competitions-data/kaggle/3004/train.csv?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1526388629&Signature=Vg3m7bEl2OS5%2Bxim%2FAPeJ%2FWfETPaNV%2Fn51Sc2bkaoKNFxb0d0P1XQ7GOXmplXgFYbgP5egw%2F%2FikMxzCOvuUyRDYDIVrViBgJ684QV%2FU6NzpfTMa6YlFrg1MInveTVGhLbRudmT9Y%2BJbFgx3LhARJPWV8MNLxzDCSnzJFUsKHWR64RGtNpPo6s3fBPp5J7lqSP9rELznnShrxycWmmPfEvpjMoxMhGmlygGVS962F4MBqXqzhu%2B6iJSlii0o%2FYOGpcs0mKc8LUsATJgfpqz2%2B81wNjBfQVz71FpyfJPljKx8BwBTE%2FDPkEgud6Y8Q5ctPg%2B6zvlwPEn3xhwfK3oattA%3D%3D"
 python gen_data.py
 python gen2.py
 
