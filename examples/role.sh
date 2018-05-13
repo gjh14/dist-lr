592e8713bf37522821ea1eb621a90beaa03d96e0
gjh14 <gjh14@tsinghua.edu.cn>
2018-05-13 16:10:04 +0800

    modify role.sh

--- examples/role.sh
+++ examples/role.sh
@@ -12,20 +12,23 @@ fi
 export RANDOM_SEED=10
 export DATA_DIR=./digit
 export NUM_FEATURE_DIM=784
-export LEARNING_RATE=0.1
 export TEST_INTERVAL=10
 export SYNC_MODE=0
-export NUM_ITERATION=200
-export BATCH_SIZE=50 # -1 means take all examples in each iteration
 
 # worker/server/scheduler settings
 export DMLC_ROLE=$1
 shift
-export UDF=$1
-shift
 export DMLC_NUM_SERVER=$1
 shift
 export DMLC_NUM_WORKER=$1
+shift
+export NUM_ITERATION=$1
+shift
+export BATCH_SIZE=$1 # -1 means take all examples in each iteration
+shift
+export LEARNING_RATE=$1
+shift
+export UDF=$1
 shift
 bin=$1
 shift
